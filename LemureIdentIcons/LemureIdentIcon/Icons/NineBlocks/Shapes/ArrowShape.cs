55d4207cdf855bbc91e928d84ebef184272bcdf0
stallosdev <stallos@outlook.com>
2021-03-19 20:04:23 -0300

    Add more shape

---
+++ LemureIdentIcons/LemureIdentIcon/Icons/NineBlocks/Shapes/ArrowShape.cs
@@ -0,0 +1,30 @@
+﻿using LemureIdentIcons.Elements;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace LemureIdentIcons.Icons.NineBlocks.Shapes
+{
+    internal class ArrowShape : IShape
+    {
+        public void DrawShape(Graphics canvas, ShapeDrawing shapeDrawing)
+        {
+            canvas.FillPolygon(
+                shapeDrawing.Brush,
+                CreatePoints(shapeDrawing)
+            );
+        }
+
+        private Point[] CreatePoints(ShapeDrawing shapeDrawing)
+        {
+            return new[]
+            {
+                new Point(shapeDrawing.X, shapeDrawing.Y),
+                new Point(shapeDrawing.X + (shapeDrawing.Width / 2), shapeDrawing.Y + shapeDrawing.Height),
+                new Point(shapeDrawing.X + shapeDrawing.Width, shapeDrawing.Y + shapeDrawing.Height),
+                new Point(shapeDrawing.X + shapeDrawing.Width, shapeDrawing.Y + (shapeDrawing.Height / 2))
+            };
+        }
+    }
+}
--- LemureIdentIcons/LemureIdentIcons.Unit.Test/ShapeTest.cs
+++ LemureIdentIcons/LemureIdentIcons.Unit.Test/ShapeTest.cs
@@ -70,6 +70,38 @@ namespace LemureIdentIcons.Unit.Test
             bitmap.Save("./shapes-04.jpg");
         }
 
+        [Fact]
+        public void DrawShape05()
+        {
+            Brush brush;
+            Bitmap bitmap;
+            Graphics canvas;
+
+            PrepareBitMap(out brush, out bitmap, out canvas);
+
+            IShape shape = new DiamondShape();
+            shape.DrawShape(canvas, new Elements.ShapeDrawing(brush, 0, 0, 50));
+
+            bitmap.Save("./shapes-05.jpg");
+        }
+
+        [Fact]
+        public void DrawShape06()
+        {
+            Brush brush;
+            Bitmap bitmap;
+            Graphics canvas;
+
+            PrepareBitMap(out brush, out bitmap, out canvas);
+
+            IShape shape = new ArrowShape();
+            shape.DrawShape(canvas, new Elements.ShapeDrawing(brush, 0, 0, 50));
+
+            bitmap.Save("./shapes-06.jpg");
+        }
+
+
+
         private static void PrepareBitMap(out Brush brush, out Bitmap bitmap, out Graphics canvas)
         {
             brush = new SolidBrush(Color.Black);
